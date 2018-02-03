08611afda7acd4e93b7ab00ec905624afdd0c935
David Cardozo <david@kiwicampus.com>
2018-02-03 13:05:30 -0800

    forgot nonlocal support

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 xhost +local:0
 mkdir ~/myscripts
 cp video.py ~/myscripts/
-docker run -it --rm --device=/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v ~/myscripts/:/data opencv_anaconda python /data/video.py
+docker run -it --rm --device=/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v ~/myscripts/:/data davidnet/datacapture python /data/video.py
